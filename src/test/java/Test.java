9bb3b34d85cd4cd67591a854be385b2c34164ab2
Hollowdaok <didilivsk@gmail.com>
2023-09-28 17:56:34 +0300

    add Mockito

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,7 +3,10 @@ import org.example.Order;
 import org.example.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ECommerceSystemTest {
     private Product product1;
@@ -15,25 +18,31 @@ class ECommerceSystemTest {
     public void setUp() {
         product1 = new Product(1, "Product 1", 30.0);
         product2 = new Product(2, "Product 2", 50.0);
-        cart = new Cart();
-        cart.addProduct(product1);
+
+        cart = mock(Cart.class);
+        order = mock(Order.class);
+        when(order.getStatus()).thenReturn("Pending");
+
         order = new Order(1, cart.getProducts());
     }
 
     @Test
     public void testAddProductToCart() {
+        when(cart.getProducts()).thenReturn(Arrays.asList(product1, product2));
         cart.addProduct(product2);
         assertEquals(2, cart.getProducts().size());
     }
 
     @Test
     public void testRemoveProductFromCart() {
+        when(cart.getProducts()).thenReturn(new ArrayList<>());
         cart.removeProduct(product1);
         assertEquals(0, cart.getProducts().size());
     }
 
     @Test
     public void testCreateOrderFromCart() {
+        when(cart.getProducts()).thenReturn(Arrays.asList(product1, product2));
         Order newOrder = new Order(2, cart.getProducts());
         assertEquals(cart.getProducts(), newOrder.getProducts());
     }
